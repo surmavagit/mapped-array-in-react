ebae6a90105aab49dde9a0261dd3e2e10e77f2a1
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-14 16:50:01 +0100

    create a React component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.querySelector('#root'));
+
+// I defined a React Function Component
+// called CatCard
+function CatCard() {
+  const cardsJSX = 
+  <div>
+    <h1>Hello React!</h1>
+  </div>;
+
+  return cardsJSX;
+}
+
+// I am using a React Component called
+// CatCard
 root.render(
-  React.createElement("h1", {}, "Hello React!")
+  <>
+  <CatCard />
+  <CatCard />
+  <CatCard />
+  </>
 );
