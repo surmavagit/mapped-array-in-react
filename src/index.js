d7e5695eeecab38f6e3a466d26ebe00c07b97dc5
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-02-15 13:42:53 +0100

    Added the new code as well as a lot of commentary

--- src/index.js
+++ src/index.js
@@ -40,13 +40,63 @@ function CatCard(props) {
     <img src={ props.catObject.picture } alt="" />
   </div>;
 
+// (props.catObject).name works fine
+// props.(catObject.picture) returns an error
+// so props.catObject gives us the object in the animals to adopt array, but why?
+// what is props? the object in question is stored in the array, it is not stored in any other object
+// what is passed as props? "catObject"? then the result would be catObject.catObject.name which is nonsense.
+// CatCard.catObject would make more sense, but CatCard is definitely not props, for
+// it is the constructor function for an object called CatCard, which has props as an argument.
+// Shouldn't we just pass the object as props in the first place? Shouldn't it be props.name instead of props.catObject.name?
+
   return cardsJSX;
 }
 
 // I am using a React Component called
 // CatCard
+// root.render(
+
+//   <CatCard catObject={ animalsToAdopt[0] } />
+
+// );
+
+
+// This doesn't work
+// Adding a console.log(animalObject) to the .map breaks the code completely!
+
+// root.render(
+//   animalsToAdopt.map((animalObject => {
+//     <CatCard catObject={ animalObject } />
+//   }))
+// );
+
+// This works! But weirdly, it gives us one card of Charlie, which is only the last one.
+// Which probably means that every time root.render runs it completely overwrites everything that was rendered before.
+// So it is mean to be run only once.
+
+// animalsToAdopt.forEach((animalObject) => {
+//   console.log(animalObject)
+//   root.render(
+//     <CatCard catObject={ animalObject } />
+//   )
+// })
+
+
+// We need to use .map on the array to give us an array of JSX elements instead of an array of objects!
+// And then we just render the result of .map!
+// root.render(
+//   animalsToAdopt.map(animalObject => <CatCard catObject={ animalObject } /> )
+// )
+// This works as intended! So the only mistake in the first attempt was adding curly brackets.
+// Arrow functions with curly brackets don't return anything automatically. And we needed those expressions to be returned.
+
+// There might be a way to make it work with curly brackets.
+
 root.render(
+    animalsToAdopt.map(animalObject => {
+      return <CatCard catObject={ animalObject } />;
+    })
+  );
+// Yes, this also works!  
 
-  <CatCard catObject={ animalsToAdopt[0] } />
-
-);
+console.dir(CatCard)
