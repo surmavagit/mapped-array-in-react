60e02da4cfe574be2d48a0767be07c5f76d467c2
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-15 09:47:12 +0100

    top-down: bring in array and big picture
    what we would need to do to convert the adopt-a-cat vanilla js
    applicatio to react in a top-down, big picture way

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const animalsToAdopt = [
+  {
+      name: "Lucky",
+      picture: "https://placekitten.com/200/287",
+  },
+  {
+      name: "Symba",
+      picture: "https://placekitten.com/200/139",
+  },
+  {
+      name: "Léo",
+      picture: "https://placekitten.com/200/90",
+  },
+  {
+      name: "Milo",
+      picture: "https://placekitten.com/200/194",
+  },
+  {
+      name: "Charly",
+      picture: "https://placekitten.com/200/179",
+  },
+];
+
 const root = ReactDOM.createRoot(
   document.querySelector('#root'));
 
@@ -18,9 +41,12 @@ function CatCard() {
 // I am using a React Component called
 // CatCard
 root.render(
-  <>
-  <CatCard />
-  <CatCard />
-  <CatCard />
-  </>
+  // the plan is to somehow translate the elements animalsToAdopt with its object
+  // array elements with another array like the one below that contains
+  // the corresponding cat card React Components
+  [
+    <CatCard />,
+    <CatCard />,
+    <CatCard />,
+  ]
 );
